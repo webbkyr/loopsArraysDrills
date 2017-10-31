52edfffdff989b9cde05f42db95e40877b66d735
Kayla Webb <kaylarwebb@gmail.com>
2017-10-31 16:40:38 -0400

    Filter function drill

---
+++ filterFunction.js
@@ -0,0 +1,12 @@
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter() {}
