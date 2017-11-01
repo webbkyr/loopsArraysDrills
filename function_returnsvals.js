5bd21e69f714e166a82b7b5db675ad634201c1a6
Kayla Webb <kaylarwebb@gmail.com>
2017-10-31 20:05:34 -0400

    Last drill for 10-31

--- function_returnsvals.js
+++ function_returnsvals.js
@@ -1,11 +1,14 @@
 function hazardWarningCreator(typeOfWarning) {
     let warningCounter = 0;
+
     return function(location) {
         warningCounter++;
         console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
         console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
     }
 }
+
+//bonus : how can we pluarlize 
 
 const rocksWarning = hazardWarningCreator('Rocks on the Road');
 const waterWarning = hazardWarningCreator('Road is flooded');
