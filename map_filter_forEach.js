36b68e629f972aee2228b8b513258279b533e145
Ben Parman <benparman@gmail.com>
2017-10-31 18:25:16 -0600

    Only comments in file at this point

---
+++ map_filter_forEach.js
@@ -0,0 +1,5 @@
+//Hey Kayla great job wrapping up the hazard alert exercise.  I'm having trouble with
+//my connection to Slack for some reason that's been going on for about an hour now
+//so I haven't been able to message you there.  I'm going to take a shot at the forEach, 
+//filter, and map exercise and then I'm going to look over everything we did today wtih my Mentor.
+//Hope you have a good night and see you back in class tomorrow!
